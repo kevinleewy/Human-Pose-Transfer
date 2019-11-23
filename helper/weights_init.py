fe865e534716e718e9d7daa8cb21a077418ace6d
Kevin Lee Wei Yang <kevinlee@Kevins-iMac-Pro.local>
2019-11-23 12:32:28 -0800

    Updated weight init functions to ignore DepthwiseSeparableConv blocks

--- helper/weights_init.py
+++ helper/weights_init.py
@@ -23,7 +23,7 @@ def weights_init_tflib(m):
     stdev = 0.02
     sqrt_v = sqrt(3)
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find('Conv') != -1 and not classname.startswith('DepthwiseSeparable')::
         nn.init.uniform_(m.weight.data, -stdev * sqrt_v, stdev * sqrt_v)
     elif classname.find('Linear') != -1:
         nn.init.uniform_(m.weight.data, -stdev * sqrt_v, stdev * sqrt_v)
@@ -36,7 +36,7 @@ def weights_init_tflib(m):
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find('Conv') != -1 and not classname.startswith('DepthwiseSeparable')::
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
@@ -45,7 +45,7 @@ def weights_init_normal(m):
 
 def weights_init_xavier(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find('Conv') != -1 and not classname.startswith('DepthwiseSeparable'):
         nn.init.xavier_uniform_(m.weight.data)
         if m.bias is not None:
             nn.init.constant_(m.bias.data, 0)
