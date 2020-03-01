0482dc447b67e609270d8f2efb6c7a3e88bf0571
Kevin Lee Wei Yang <kevinlee@Kevins-iMac-Pro.local>
2020-03-01 15:24:49 -0800

    lazy load npy files

--- implementations/LGAN/Datasets/GFVDataset.py
+++ implementations/LGAN/Datasets/GFVDataset.py
@@ -4,18 +4,17 @@ from torch.utils.data import Dataset
 
 class GFVDataset(Dataset):
     def __init__(self, config):
-        self.data_path = config["dataset"]["path"]["train"]["image"]
-        self.imsize = config["model"]["lgan"]["imsize"]
+        self.dataset_path = config["dataset"]["path"]["train"]["image"]
 
         self.dataset = []
-        for file in os.listdir(self.data_path):
+        for file in os.listdir(self.dataset_path):
             if file.endswith(".npy"):
-                data = np.load(os.path.join(self.data_path, file))
-                self.dataset.append(data)
+                data_path = os.path.join(self.dataset_path, file)
+                self.dataset.append(data_path)
 
     def __getitem__(self, index):
-        data = self.dataset[index]
-        # data = data.reshape(-1, self.imsize, self.imsize)
+        data_path = self.dataset[index]
+        data = np.load(data_path)
         return data
 
     def __len__(self):
