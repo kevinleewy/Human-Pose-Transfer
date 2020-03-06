d643f1a2e11da111e194d037cf430ac4b6b026c2
Kevin Lee Wei Yang <kevinlee@Kevins-iMac-Pro.local>
2020-03-05 20:56:22 -0800

    corrected logic for adding to ReplayBuffer

--- implementations/RL/utils.py
+++ implementations/RL/utils.py
@@ -111,23 +111,29 @@ def normalize_data(data, mean, std):
 
 # Simple replay buffer
 class ReplayBuffer(object):
-	def __init__(self):
-		self.storage = []
+    def __init__(self, size=20000):
+        self.storage = []
+        self.size = size
+        self.next_index = 0
 
 	# Expects tuples of (state, next_state, action, reward, done)
-	def add(self, data):
-		self.storage.append(data)
+    def add(self, data):
+        if self.next_index >= len(self.storage):
+            self.storage.append(data)
+        else:
+            self.storage[self.next_index] = data
+        self.next_index = (self.next_index + 1) % self.size
 
-	def sample(self, batch_size=100):
-		ind = np.random.randint(0, len(self.storage), size=batch_size)
-		x, y, u, r, d = [], [], [], [], []
+    def sample(self, batch_size=100):
+        ind = np.random.randint(0, len(self.storage), size=batch_size)
+        x, y, u, r, d = [], [], [], [], []
 
-		for i in ind:
-			X, Y, U, R, D = self.storage[i]
-			x.append(np.array(X, copy=False))
-			y.append(np.array(Y, copy=False))
-			u.append(np.array(U, copy=False))
-			r.append(np.array(R, copy=False))
-			d.append(np.array(D, copy=False))
+        for i in ind:
+            X, Y, U, R, D = self.storage[i]
+            x.append(np.array(X, copy=False))
+            y.append(np.array(Y, copy=False))
+            u.append(np.array(U, copy=False))
+            r.append(np.array(R, copy=False))
+            d.append(np.array(D, copy=False))
 
-		return np.array(x), np.array(y), np.array(u), np.array(r).reshape(-1, 1), np.array(d).reshape(-1, 1)
+        return np.array(x), np.array(y), np.array(u), np.array(r).reshape(-1, 1), np.array(d).reshape(-1, 1)
